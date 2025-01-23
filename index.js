c7026344178ee22ba845e8f6920068391a60482b
Rogério Ramos Maldonado <rogerioramosm@gmail.com>
2025-01-23 15:23:54 -0400

    fix: correções de total de arquivo da lixeira

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { deleteFile } from './src/function/deleteFile.js';
 import { loadData } from './src/function/loadData.js';
 
-// Informe aqui a quantidade de arquivos que existem na sua lixeira
-// Esta informação pode receber executando uma vez a API de leitura da lixeira.
-let totalFilesToDelete = 30388;
+/**
+ * Esta variável irá receber o total de arquivos existentes na lixeira, na primeira iteração do loop While.
+ * Inicia-se a variável com 1 para entrar no loop e fazer a verificação uma primeira vez.
+ */
+let totalFilesToDelete = 1;
 
 // Quantidade de arquivos que serão excluidos por vez. Nos testes, 400 foi um número que não causou timeout
 const filesForDelete = 400;
@@ -12,10 +14,18 @@ while (totalFilesToDelete > 0) {
   // Carrega arquivos na lixeira Alfresco através da API
   const loadDataFromApi = await loadData(0, filesForDelete);
 
-  if (loadDataFromApi) {
+  // Atualiza informação da quantidade de arquivos que existem na lixeira
+  totalFilesToDelete = loadDataFromApi.totalItems;
+
+  if (totalFilesToDelete <= 0) {
+    console.log("Não existem arquivos na lixeira para limpar!");
+    break;
+  }
+
+  if (loadDataFromApi.files) {
     try {
       await deleteFile(loadDataFromApi);
-      
+
       // Decrementa a quantidade de arquivos que são lidos por vez do total existente
       totalFilesToDelete -= filesForDelete;
 
